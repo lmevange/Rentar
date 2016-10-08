d1230ef7eb8432f56b2b8a72d82697b95e1ffaa1
cascioli <joe.cash30@gmail.com>
2016-10-08 05:35:35 +0000

    restructured and updated to python 3.5

---
+++ server/rentar/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+	return render_to_response('../../index.html')

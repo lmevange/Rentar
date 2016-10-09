3fbdd3ec0905da41cc817a7437ad3d7f32f7ba65
cascioli <joe.cash30@gmail.com>
2016-10-09 00:06:23 +0000

    Set up individual views, but until we change our pages to use django links, pictures, and css inside the pages will not work

--- server/rentar/urls.py
+++ server/rentar/urls.py
@@ -1,9 +1,10 @@
 
 from django.conf.urls import url
 from . import views
-#from django.views.generic import TemplateView
 
 urlpatterns = [
+#	url(r'^$', views.addressview, name='addressview'),
+#	url(r'^$', views.contact, name='contact'),
+#	url(r'^$', views.login, name='login'),
 	url(r'^$', views.index, name='index'),
-#	url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
 ]
--- server/rentar/views.py
+++ server/rentar/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import Context, loader
+
 # Create your views here.
 def index(request):
-#	return HttpResponse("This is a test")
-#	return render_to_response('../../index.html')
-#	myFile = open('index.html')-------------------------second try, no luck cant find file
-#	data="\n".join(line for line in myFile)
-#	return HttpResponse(data)
-#	template = loader.get_template("index.html")
-#	return HttpResponse(template.render())
 	return render(request, 'index.html')
+
+def login(request):
+	return render(request, 'login.html')
+
+def addressview(request):
+	return render(request, 'addressview.html')
+
+def contact(request):
+	return render(request, 'contact.html')
+

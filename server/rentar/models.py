4945882cd0a8dd35fd90eedcd890ace500b774cb
lmevange <lmevange@buffalo.edu>
2016-10-22 22:56:45 +0000

    added input options to apartment

--- server/rentar/models.py
+++ server/rentar/models.py
@@ -2,15 +2,16 @@ from django.db import models
 
 class Apartment(models.Model):
 	address_text = models.CharField(max_length=250)
-	years_lived = models.DurationField('Years lived in')
+	move_in = models.DateField('Moved in')
+	years_lived = models.PositiveSmallIntegerField('Years lived in')
 	security_deposit = models.IntegerField(default=0)
 
-	 def __str__(self):
-        return self.address_text
+	def __str__(self):
+	   return self.address_text
 
 class LandLord(models.Model):
 	name_text = models.CharField(max_length=200)
 
 	def __str__(self):
-	   return self.address_text
+	   return self.name_text
 	

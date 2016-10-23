616d00b8a08d52abe0aad096e59301e407181328
cascioli <joe.cash30@gmail.com>
2016-10-23 17:53:52 -0400

    set up hierarchy for templates and static files

--- server/rentar/urls.py
+++ server/rentar/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-#	url(r'^$', views.addressview, name='addressview'),
-#	url(r'^$', views.contact, name='contact'),
-#	url(r'^$', views.login, name='login'),
+	url(r'^addressview/$', views.addressview, name='addressview'),
+	url(r'^contact/$', views.contact, name='contact'),
+	url(r'^login/$', views.login, name='login'),
 	url(r'^$', views.index, name='index'),
 ]

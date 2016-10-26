a59fc64f7383cb8e15ca27e2e062754b039bcf91
cascioli <joe.cash30@gmail.com>
2016-10-25 23:36:06 -0400

    expanded apartment, landlord and apartment_ratings

--- server/rentar/models.py
+++ server/rentar/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Apartment(models.Model):
 	#location info
-	full_address = models.CharField(max_length=250,primary_key=True)
+	address_full = models.CharField(max_length=250,primary_key=True)
 	address_line = models.CharField(max_length=250)
 	city = models.CharField(max_length=250)
 	state = models.CharField(max_length=250)
@@ -24,26 +24,57 @@ class Apartment(models.Model):
 	washer = models.BooleanField(default=False)
 	dryer = models.BooleanField(default=False)
 	#parking info
-	parking_onstreet = models.BooleanField()
-	parking_offstreet = models.BooleanField()
+	parking_onstreet = models.BooleanField(default=False)
+	parking_offstreet = models.BooleanField(default=False)
 	#public transportation
-	public_transportation= models.BooleanField()
-	#add average rent and/or security deposit
+	public_transportation= models.BooleanField(default=False)
+	#pets later add average fees
+	pets = models.booleanField(default=False)
+	#landlord info
+	landlord = models.Foreignkey(LandLord) #should only have one
+	#rating info
+	ratings = models.ManyToManyField(Apartment_Ratings)
 
-	#landlord info (should only be on one table, chose this casue it is the form)
-	landlords = models.ManyToManyField(LandLord)
-
-	years_lived = models.PositiveSmallIntegerField('Years lived in')
-	security_deposit = models.IntegerField(default=0)
+	avg_security_deposit = models.DecimalField(max_digits=5, decimal_places=2)
+	avg_rent = models.DecimalField(max_digits=6, decimal_places=2)
 
 	def __str__(self):
-	   return self.address_line
+	   return self.address_full
+
+##################
+#id with auto inc is set as primary key automatically for all tables
+##################
 
 class Landlord(models.Model):
-	name_text = models.CharField(max_length=200)
-	properties = models.
+	#id_auto_inc = models.AutoField(primary_key =True)
+	first_name = models.CharField(max_length=200)
+	last_name = models.CharField(max_length=200)
+	company = models.CharField(max_length=200)
 	def __str__(self):
 	   return self.name_text
 	
 class Apartment_Rating(models.Model):
-	
+	#id_auto_inc = models.AutoField(primary_key =True)
+	#info on rent
+	apartment = models.Foreignkey(Apartment, on_delete=models.CASCADE) #if apartment is deleted delete this too
+	landlord = models.Foreignkey(Landlord)
+	move_in_date = models.DateField()
+	years_lived = models.PositiveSmallIntegerField(default=1)
+	#costs
+	starting_rent = models.DecimalField(max_digits=5, decimal_places=2)
+	ending_rent = models.DecimalField(max_digits=5, decimal_places=2)
+	security_deposit = models.DecimalField(max_digits=5, decimal_places=2)
+	pet_fee = models.DecimalField(max_digits=5, decimal_places=2, default= 0)
+	#ratings
+	#utilities:
+	water = models.IntegerField(choices=[(i, i) for i in range(1, 5)], blank=True)
+	heat = models.IntegerField(choices=[(i, i) for i in range(1, 5)], blank=True)
+	electric = models.IntegerField(choices=[(i, i) for i in range(1, 5)], blank=True)
+	garbage = models.IntegerField(choices=[(i, i) for i in range(1, 5)], blank=True)
+	#general
+	parking = models.IntegerField(choices=[(i, i) for i in range(1, 5)], blank=True)
+	privacy = models.IntegerField(choices=[(i, i) for i in range(1, 5)], blank=True)
+	location = models.IntegerField(choices=[(i, i) for i in range(1, 5)], blank=True)
+	maintenance = models.IntegerField(choices=[(i, i) for i in range(1, 5)], blank=True)
+	landlord = models.IntegerField(choices=[(i, i) for i in range(1, 5)], blank=True)
+	comment = models.TextField(max_length = 2000)

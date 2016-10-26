c32c6354b505c20ab37114adc7cf945471a42e3c
lmevange <leo327-tes@hotmail.com>
2016-10-26 18:21:41 -0400

    quick fix 2
    quicky

--- server/rentar/views.py
+++ server/rentar/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 # from django.forms import ApartmentForm
-from django.models import ApartmentForm
+from models import ApartmentForm
 
 # Create your views here.
 def index(request):

a601b29c0e272f54ed95434e4d84fe4a6f3191eb
lmevange <leo327-tes@hotmail.com>
2016-10-27 20:30:56 -0400

    Title test
    testing how to give fields titles, me and joe will do the rest of the work on his computer since we dont hhave to push everytime to test on it

--- server/rentar/models.py
+++ server/rentar/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Apartment(models.Model):
-	address_text = models.CharField(max_length=250)
+	address_text = models.CharField(label = 'Adress', max_length=250)
 	years_lived = models.CharField('Years lived in',max_length=250)
 	security_deposit = models.IntegerField(default=0)
 
 class Landlord(models.Model):
 	name_text = models.CharField(max_length=200)
-	
+
 # Create your models here.

fe72003a58adf7b6fef6867dd0e64d065572b1ce
cascioli <joe.cash30@gmail.com>
2016-10-27 22:34:33 -0400

    Added defaults to models

--- server/rentar/models.py
+++ server/rentar/models.py
@@ -1,22 +1,22 @@
 from django.db import models
 
 class Landlord(models.Model):
-	first_name = models.CharField(default="John", max_length=200)
-	last_name = models.CharField(default="Doe", max_length=200)
+	first_name = models.CharField(default="", max_length=200)
+	last_name = models.CharField(default="", max_length=200)
 	company = models.CharField(max_length=200)
 	def __str__(self):
 	   return self.last_name
 
 class Apartment(models.Model):
 	#location info
-	address_full = models.CharField(max_length=250,primary_key=True)
-	address_line = models.CharField(max_length=250)
-	city = models.CharField(max_length=250)
-	state = models.CharField(max_length=250)
-	zipcode = models.CharField(max_length=250)
+	#address_full = models.CharField(default="",max_length=250,primary_key=True)
+	address_line = models.CharField(default="",max_length=250)
+	city = models.CharField(default="",max_length=250)
+	state = models.CharField(default="",max_length=250)
+	zipcode = models.CharField(default="",max_length=250)
 	#generic info
-	bedrooms = models.CharField(max_length=250)
-	bathrooms = models.CharField(max_length=250)
+	bedrooms = models.CharField(default="",max_length=250)
+	bathrooms = models.CharField(default="",max_length=250)
 	living_room = models.BooleanField(default=False)
 	dining_room = models.BooleanField(default=False)
 	basement = models.BooleanField(default=False)
@@ -42,8 +42,8 @@ class Apartment(models.Model):
 	#rating info
 	#ratings = models.ManyToManyField(Apartment_Ratings)
 
-	avg_security_deposit = models.DecimalField(max_digits=5, decimal_places=2)
-	avg_rent = models.DecimalField(max_digits=6, decimal_places=2)
+	avg_security_deposit = models.DecimalField(default=0,max_digits=5, decimal_places=2)
+	avg_rent = models.DecimalField(default=0,max_digits=6, decimal_places=2)
 
 	def __str__(self):
 	   return self.address_full
@@ -57,14 +57,14 @@ class Apartment_Rating(models.Model):
 	landlord = models.ForeignKey(Landlord)
 
 	move_in_date = models.DateField()
-	years_lived = models.PositiveSmallIntegerField(default=1)
+	years_lived = models.PositiveSmallIntegerField(default=0)
 	#costs
-	starting_rent = models.DecimalField(max_digits=5, decimal_places=2)
-	ending_rent = models.DecimalField(max_digits=5, decimal_places=2)
-	security_deposit = models.DecimalField(max_digits=5, decimal_places=2)
-	pet_fee = models.DecimalField(max_digits=5, decimal_places=2, default= 0)
+	starting_rent = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+	ending_rent = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+	security_deposit = models.DecimalField(default=0,max_digits=5, decimal_places=2)
+	pet_fee = models.DecimalField(default=0, max_digits=5, decimal_places=2, default= 0)
 	#ratings
-	#utilities:
+	#utilities:default=0, 
 	water = models.IntegerField(choices=[(i, i) for i in range(1, 6)], blank=True)
 	heat = models.IntegerField(choices=[(i, i) for i in range(1, 6)], blank=True)
 	electric = models.IntegerField(choices=[(i, i) for i in range(1, 6)], blank=True)

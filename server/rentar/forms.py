dce828293780ac0064fa7bb3d2b2fbf6d0ee2cc3
thelordmalcolm <lil_mal0@yahoo.com>
2016-11-05 18:28:16 -0400

    1st commit
    Temp

--- server/rentar/forms.py
+++ server/rentar/forms.py
@@ -1,20 +1,37 @@
+from django.contrib.auth.models import User
 from django import forms
 from rentar.models import Apartment, Landlord, Apartment_Rating, Landlord_Rating
+
+
 class ApartmentForm(forms.ModelForm):
-	class Meta:
-		model = Apartment
-		fields = '__all__'
+    class Meta:
+        model = Apartment
+        fields = '__all__'
+
+
 class LandlordForm(forms.ModelForm):
-	class Meta:
-		model = Landlord
-		fields = '__all__'
+    class Meta:
+        model = Landlord
+        fields = '__all__'
+
 
 class ApartmentRatingForm(forms.ModelForm):
-	class Meta:
-		model = Apartment_Rating
-		fields = '__all__'
+    class Meta:
+        model = Apartment_Rating
+        fields = '__all__'
+
 
 class LandlordRatingForm(forms.ModelForm):
-	class Meta:
-		model = Landlord_Rating
-		fields = '__all__'
+    class Meta:
+        model = Landlord_Rating
+        fields = '__all__'
+
+
+#User profile information
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+		model = User
+		fields = ['username','email', 'password']

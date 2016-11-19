1e33223c3e04b853c9b77c4037470dc8fba4758c
lmevange <leo327-tes@hotmail.com>
2016-11-18 22:37:59 -0500

    adding imports

--- server/rentar/views.py
+++ server/rentar/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.forms import ModelForm

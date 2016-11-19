33bf77c17bf3c94730d028038ad420967ff4ad88
lmevange <leo327-tes@hotmail.com>
2016-11-18 22:44:16 -0500

    Ability to pass keys

--- server/rentar/urls.py
+++ server/rentar/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^add_apartment_rating/$', views.add_apartment_rating, name='add_apartment_rating'), # NEW MAPPING!
+	url(r'^add_apartment_rating/(?P<pk>\d+)$', views.add_apartment_rating, name='add_apartment_rating'), # NEW MAPPING!
 	url(r'^add_apartment/$', views.add_apartment, name='add_apartment'), # NEW MAPPING!
 	url(r'^addressview/$', views.addressview, name='addressview'),
 	url(r'^contact/$', views.contact, name='contact'),

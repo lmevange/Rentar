26427917ba8a77d549ff84cacb8d1a3e9e58430d
lmevange <leo327-tes@hotmail.com>
2016-11-18 22:08:52 -0500

    More fixing after combine

--- server/rentar/forms.py
+++ server/rentar/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from rentar.models import Apartment, Landlord, Apartment_Rating, Landlord_Rating
+from rentar.models import Apartment, Apartment_Rating
 class ApartmentForm(forms.ModelForm):
 	class Meta:
 		model = Apartment
